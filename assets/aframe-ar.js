ce3467ccef57054a04fb5922f6a08a1843c8b43b
Mingxuan-Xie <31252716+CrabMX@users.noreply.github.com>
2019-05-22 22:29:45 +0800

    Add web browser tips

--- assets/aframe-ar.js
+++ assets/aframe-ar.js
@@ -5937,7 +5937,7 @@ ARjs.Source.prototype._initSourceWebcam = function(onReady, onError) {
 
 	// init default value
 	onError = onError || function(error){	
-		alert('微信不支持AR哦\nName: '+error.name + '\nMessage: '+error.message)
+		alert('微信暂不支持AR网页哦'+'\n'+error.message)
 	}
 
 	var domElement = document.createElement('video');
@@ -5957,7 +5957,7 @@ ARjs.Source.prototype._initSourceWebcam = function(onReady, onError) {
 		else console.assert(false)
 		onError({
 			name: '',
-			message: '请点击右上角使用手机自带浏览器（Safari）打开'
+			message: '请点击右上角使用手机自带浏览器(Safari)打开，并允许网页访问相机~'
 		})
 		return null
 	}
